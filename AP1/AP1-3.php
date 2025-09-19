640b3fb0da2caf5d9485170ca350b65a4d77be5c
laleexs <alex.sueca.@gmail.com>
2025-09-19 22:50:38 +0200

    AP1-3 per fer

---
+++ AP1/AP1-3.php
@@ -0,0 +1,16 @@
+<?php
+// tenemos que decir de que tipo son los datos
+declare(strict_types=1);
+
+$datos = array(
+    1 => "primero",
+    3 => "segundo",
+    5 => "tercero",
+    7 => "cuarto",
+    9 => "quinto",
+    11 => "sexto",
+);
+
+$par = 0;
+$impar = 0;
+
