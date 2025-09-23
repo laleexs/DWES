ceadc748973d4eb8cfe4d1cacedab5af377a737c
laleexs <alex.sueca.3@gmail.com>
2025-09-23 18:15:28 +0200

    AP1-3 acavada

--- AP1/AP1-3.php
+++ AP1/AP1-3.php
@@ -11,6 +11,37 @@ $datos = array(
     11 => "sexto",
 );
 
-$par = 0;
-$impar = 0;
+
+(bool)$par = false;
+(bool)$impar = true;
+
+(int)$total = 0;
+(int)$iteracion = 0;
+
+foreach ($datos as $key => $value) {
+    if(((++$iteracion) % 2) == 0){  // le sumamos 1 a la iteracion antes de calcular nada
+        echo "Estas en una posicion par <br>";
+        $impar = false;
+        $par = true;
+    }else {
+        echo "Estas en una posicion impar <br>";
+        $par = false;
+        $impar = true;
+    }
+
+    $total += $key;
+    
+    echo "la suma de las claves";
+
+    if($total > 20) {
+        echo "el valor es mayor que 20<br>";
+    }elseif($total > 10) {
+        echo "el valor es mayor que 20<br>";
+    }elseif($total > 5) {
+        echo "el valor es mayor que 5<br>";
+    }else {
+        echo "el valor es menor que 5<br>";
+    }
+
+}
 
