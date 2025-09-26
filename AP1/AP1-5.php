6737ef039592554754fb65f38242fd3548e0a960
laleexs <alex.sueca.3@gmail.com>
2025-09-26 20:00:52 +0200

    AP1-5 acavat pero ix duplicat el ultim usuari

--- AP1/AP1-5.php
+++ AP1/AP1-5.php
@@ -33,7 +33,7 @@ if ($result->num_rows > 0) {
 }
 //insertar usuario en la bbdd
 
-$sql = "INSERT INTO usuarios(nombre, estado) values(Alex, false)";
+$sql = "INSERT INTO usuarios(nombre, estado) values('Alexandre', false)"; // los valores entre comillas simples no dobles
 
 // try and catch para controlar si en la sentecia hay algún error
 try {
@@ -41,5 +41,46 @@ try {
     $id = $conn->insert_id;
     echo "Se a realizado con exito la inserción de la nueva id: " . $id . "<br>";
 
-} catch (mysqli_sql_exception $e)
+} catch (mysqli_sql_exception $e){
+    die ("Se ha producido el siguiente error:<br>" . $e->getMessage() . " En la linea: " . $e->getLine() . "<br>");
+}
 
+//realizamos la actualización
+
+$sql = "UPDATE usuarios SET estado =true WHERE id= " .$id;
+
+try {
+    $conn->query($sql);
+    echo "Se ha realizado correctamente la actualización de la id:" . $id ."<br>";
+} catch (mysqli_sql_exception $e){
+    die("Se ha producido el siguiente error:" . $e->getMessage() . " En la Línea: " . $e->getLine() . "<br>");
+}
+
+// sql para mostrar los datos después de actualizarlos
+
+$sql= "SELECT * FROM usuarios";
+// lanzamos consulta para devolver resultados
+$result  = $conn->query($sql);
+
+if ($result->num_rows > 0){
+    while ($row =  $result->fetch_assoc()) {
+        echo " El usuario " . $row['nombre'] . " posee la id: " . $row['id'] . "y su estado es: " . $row['estado'] . "<br>";
+    }
+}else {
+    echo " 0 resultados obtenidos";
+}
+
+
+//borrado de un dato
+
+$sql= "DELETE FROM usuarios WHERE id=" . $id ;
+
+try {
+    $conn->query($sql);
+    echo "Se ha realizado correctamente el borrado de la id:" . $id ."<br>";
+} catch(mysqli_sql_exception $e) {
+    die("Se ha producido el siguiente error:" . $e->getMessage() . " En la Línea: " . $e->getLine() . "<br>");
+}
+
+//  IMPORTANTE CERRAR LA CONEXION PARA EVITAR CONSUMIR RECURSOS
+$conn->close();
