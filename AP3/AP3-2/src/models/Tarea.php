761dc19efe0719e5cb180116c76bc855c99003c0
laleexs <alex.sueca.3@gmail.com>
2025-10-10 22:28:27 +0200

    creando clase Tarea que recoge la data creando instancia y enviando la sentencia SQL

--- AP3/AP3-2/src/models/Tarea.php
+++ AP3/AP3-2/src/models/Tarea.php
@@ -1,1 +1,12 @@
 <?php
+require_once (__DIR__ . '/../core/Database.php');
+
+class Tarea
+{
+    public function getData()
+    {
+        $conn = Database::getInstance(); // acedemos a la funcion de la clase database porque la clase database tiene propiedades estaticas a las cuales podemos aceder a traves del ::
+        $data = $conn->executeSQL("SELECT * FROM tareas");
+        return $data;
+    }
+}
