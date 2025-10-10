c6b2df0db08ba2bc8b8b566e774f5ae36741c062
laleexs <alex.sueca.3@gmail.com>
2025-10-10 22:44:58 +0200

    pequeño html mas o menos como en el ejercicio anterior para mostrar las tareas

--- AP3/AP3-2/src/views/ListadoTareas.php
+++ AP3/AP3-2/src/views/ListadoTareas.php
@@ -1,1 +1,16 @@
 <?php
+
+class ListadoTareas
+{
+    private array $tareas;
+    public function render( array $tareas =null): void
+    {
+        $this->tareas = $tareas; // guarda la data que le envia el controlador para poder mostrarla
+        ?>
+        <h1><?= $this->tareas['id'] ?></h1>
+        <h2><?= $this->tareas['descripcion'] ?></h2>
+        <p><?= $this->tareas['fecha_creacion'] ?></p>
+        <p><?= $this->tareas['fecha_vencimiento'] ?></p>
+        <?php
+    }
+}
