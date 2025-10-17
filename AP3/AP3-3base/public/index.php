1472b7aa4f8398cad06f911f763ef0f520e07c15
laleexs <alex.sueca.3@gmail.com>
2025-10-17 15:53:13 +0200

    AP3-3 segun video explicativo solucion

--- AP3/AP3-3base/public/index.php
+++ AP3/AP3-3base/public/index.php
@@ -8,8 +8,10 @@ use AP33\Core\{Dispatcher, RouteCollection, Request};
 
 //Creamos un objeto que contenga todas las rutas de la aplicación.
 $route = new RouteCollection();
+
 //Creamos un objeto que contenga la ruta que hemos recibido desde el navegador.
 $request = new Request();
+
 //Ahora creamos un objeto que se encarga de redirigir al controller que corresponda la aplicación
 $dispacher = new Dispatcher($route, $request);
 
